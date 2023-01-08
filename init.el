6996b4b471dd41fbbb76d95b3eafea5c42c41974
Edenir Anschau <edenir.ans@gmail.com>
2023-01-08 10:38:28 -0300

    Set up Org mode and add packages(org-bullets and magit)

--- init.el
+++ init.el
@@ -28,12 +28,32 @@
   :config
   (load-theme 'zenburn t))
 
+(use-package magit
+  :ensure t
+  :config)
+
+;; Org mode stuff
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "|" "DONE")))
+
+(setq org-todo-keyword-faces
+      '(("IN-PROGRESS" . "yellow")))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme company which-key use-package)))
+ '(package-selected-packages
+   '(magit org-bullets org-bulllets zenburn-theme company which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
